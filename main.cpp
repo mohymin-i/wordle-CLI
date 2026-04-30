0b93ba44e48b8ea3440a0685aa3aee9a9c6376b4
mohymin-i <mohymini@umich.edu>
2026-04-30 03:27:49 -0400

    Did some more stuff

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <string> 
 #include <random>
+#include <fstream> 
 
 using std::cout;
 using std::cin;
-using std::string; 
+using std::string;
+
 
 class Wordle {
 private:
@@ -13,12 +15,55 @@ public:
 
 }; // Wordle
 
+
 int main() {
-    bool playing = true; 
+    // bool playing = true; 
 
-    while (playing) {
+    // while (playing) {
 
-    }
+    // }
 
     return; 
+}
+
+
+
+// helper functions
+
+// loops through the inputFile, checks for all words of length wordSize, adds them to the new wordlist
+// returns an integer fileLength that tells us the file 
+int stripToTxt(string inputFile, string outputFile, int wordSize) {
+    std::ifstream input(inputFile);
+    std::ofstream output(outputFile);
+    int fileLength = 0; 
+    string curr; 
+
+    while (getline(input, curr)) {
+        if (verifyWord(curr, wordSize)) {
+            output << curr << "\n";
+            fileLength++; 
+        }
+    }
+
+    input.close();
+    output.close();
+    return fileLength;
+}
+
+// takes in a word and a length
+// verifies that the word is the proper length
+// also makes sure that we don't add any swears by accident
+// swears are flagged with '!' at the back
+bool verifyWord(string word, int length) {
+    if (word.size() != length) {
+        return false;
+    } else {
+        for (char c : word) {
+            if (c == '!') {
+                return false;
+            }
+        }
+    }
+
+    return true; 
 }
